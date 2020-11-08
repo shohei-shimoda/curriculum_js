18a11ca59d0e3eb55f6b13ae69b97a2ed723d650
shohei-shimoda <s.tel-9527@docomo.ne.jp>
2020-11-08 23:44:13 +0900

    FIZZBUZZ修正
    修正１

---
+++ JavaScript/1-7/main.js
@@ -0,0 +1,17 @@
+//「山田」オブジェクト作成
+let yamada = { 
+  name: 'yamada',
+  height: 170,
+  weight: 60,
+  gender: "男",
+  age: 30,
+}
+console.log(yamada);
+//「鈴木」オブジェクト作成
+let suzuki = {
+  name: 'suzuki',
+  height: 180,
+  weight: 70,
+  gender: "男",
+  age: 35,
+}
